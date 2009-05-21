5690bd404719758bda46bae38609f72c0ff66b44
Justin <justin@presidentbeef.com>
2009-05-20 21:26:53 -0700

    Changes to array methods to deal with empty arrays
    -Made empty arrays special cases
    -Besides, it was broken before
    -Added test cases for empty arrays

--- test/test-core.rb
+++ test/test-core.rb
@@ -8,15 +8,22 @@ class BratCoreTests < Test::Unit::TestCase
 
 	def test_core_array_map
 		assert_result "[2,3,4]", "a = [1,2,3]; a.map {i| i + 1 }"
+		assert_result "[]", "a = []; a.map {i| i + 1 }"
 	end
 
 	def test_core_array_map_with_index
 		assert_result "[[0,a],[1,b],[2,c]]", "b = ['a, 'b, 'c]; b.map_with_index { obj, index | [index, obj] }"
+		assert_result "[]", "a = []; a.map_with_index {i, in | i + 1 }"
 	end
 
 	def test_core_array_first
 		assert_result "1", "a = [1,2,3]; a.first"
 		assert_result "1", "[1,2,3].first"
+	end
+
+	def test_core_array_last
+		assert_result "3", "a = [1,2,3]; a.last"
+		assert_result "3", "[1,2,3].last"
 	end
 
 	def test_core_array_rest
@@ -26,10 +33,12 @@ class BratCoreTests < Test::Unit::TestCase
 
 	def test_core_array_each
 		assert_result "[1,2,3]", "b = []; [1,2,3].each { i | b << i }; b "
+		assert_result "[]", "b = []; [].each { i | b << i }; b "
 	end
 
 	def test_core_array_each_with_index
 		assert_result "[0,a,1,b,2,c]", "a = [];b = ['a, 'b, 'c]; b.each_with_index { obj, index | a << index << obj }; a"
+		assert_result "[]", "b = []; [].each_with_index { i, in | b << i }; b "
 	end
 
 	def test_core_array_empty
@@ -40,36 +49,37 @@ class BratCoreTests < Test::Unit::TestCase
 
 	def test_core_array_select
 		assert_result "[1,2]", "[1,2,3].select { i | i < 3 }"
+		assert_result "[]", "[].select { i | i < 3 }"
 	end
 
 	def test_core_array_include?
 		assert_result "true", "[1,2,3].include? 3"
 		assert_result "true", "a = new; b = [a]; b.include? a"
 		assert_result "false", "a = 1; b = [a]; b.include? 2"
+		assert_result "false", "a = 1; b = []; b.include? 2"
 	end
 
 	def test_core_array_any?
 		assert_result "true", "[1,2,3].any? { x | x > 2 }"
 		assert_result "false", "[1,2,3].any? { x | x > 4 }"
-	end
-
-	def test_core_array_any?
-		assert_result "false", "[1,2,3].all? { x | x > 2 }"
-		assert_result "true", "[1,2,3].all? { x | x < 4 }"
+		assert_result "false", "[].any? { x | x > 4 }"
 	end
 
 	def test_core_array_find
 		assert_result "3", "[1,2,3].find { x | x > 2 }"
 		assert_result "null", "[1,2,3].find { x | x > 4 }"
+		assert_result "null", "[].find { x | x > 4 }"
 	end
 
 	def test_core_while
 		assert_result "3", "n = 0; while { n = n + 1; n < 3 }; n"
+		assert_result "3", "n = 0; while { n < 3 }, { n = n + 1 }; n"
 	end
 
 	def test_core_number_times
 		assert_result "5", "5.times { 'a }"
 		assert_result "[a,a,a]", "a = []; 3.times { a << 'a }; a"
+		assert_result "[]", "a = []; 0.times { a << 'a }; a"
 	end
 
 	def test_core_and
