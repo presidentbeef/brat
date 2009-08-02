994c3bba214ee367ab373626144b2f5c0acd817c
Justin <justin@presidentbeef.com>
2009-08-02 05:30:32 -0700

    Use local libneko when compiling extensions

--- lib/src/build.sh
+++ lib/src/build.sh
@@ -1,6 +1,6 @@
-gcc -fPIC -lgmp -lneko -pthread -c number.c
-gcc -shared -lgmp -lneko -o ../number.ndll number.o
+gcc -L ../ -fPIC -lgmp -lneko -pthread -c number.c && \
+gcc -L ../ -shared -lgmp -lneko -o ../number.ndll number.o && \
 rm number.o
-gcc -fPIC -lneko -pthread -c core-util.c
-gcc -shared -lneko -o ../core-util.ndll core-util.o
+gcc -L ../ -fPIC -lneko -pthread -c core-util.c && \
+gcc -L ../ -shared -lneko -o ../core-util.ndll core-util.o && \
 rm core-util.o
