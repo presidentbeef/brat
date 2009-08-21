1f6f06023602edcb792ab6173720e9f63b817062
Justin <justin@presidentbeef.com>
2009-08-20 17:56:05 -0700

    Changed comparison behavior to not rely on <=>
    Basically, because <=> and == need to be different, unfortunately.
    Also, fixed array comparison to non-arrays.

--- test/test.rb
+++ test/test.rb
@@ -686,10 +686,10 @@ class BratParserTest < Test::Unit::TestCase
 		assert_result "true", "a = 300; b = 200; b + 100 == a"
 		assert_result "true", "a = 300; b = 200; b + 300 == a + 200"
 		assert_result "false", "[1,2,3].length <= 1"
-		assert_result "false", '1 < "1"'
+		assert_fail '1 < "1"'
 		assert_result "false", '1 == "1"'
 		assert_result "true", '1 != "1"'
-		assert_result "false", '1 >= "1"'
+		assert_fail '1 >= "1"'
 	end
 
 	def test_bignum_compare
