e751dd7535dcfd83e238b467b4e20cf74960ae9c
Justin Collins <justin@presidentbeef.com>
2019-01-31 14:47:37 -0800

    Add base64 library for encoding/decoding Base64
    Apparently I wrote this a long time ago, but didn't commit it.

---
+++ stdlib/base64.lua
@@ -0,0 +1,21 @@
+require("mime")
+
+local base64 = object:new()
+
+function base64:encode (str)
+  if type(str) == "table" and str._lua_string then
+    return base_string:new(mime.b64(str._lua_string))
+  else
+    return base_string:new(mime.b64(tostring(str._lua_string)))
+  end
+end
+
+function base64:decode (str)
+  if type(str) == "table" and str._lua_string then
+    return base_string:new(mime.unb64(str._lua_string))
+  else
+    return base_string:new(mime.unb64(tostring(str._lua_string)))
+  end
+end
+
+object:export(base64, "base64")
